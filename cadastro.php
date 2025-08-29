1a3aa7ea940d625ed22270f671be83f9286e8dfb
clemente7stfw <gabiclemente2007@gmail.com>
2025-08-29 15:22:47 -0300

    Deletendo CSS separado das paginas de login e cadastro e adicionando CSS interno

--- cadastro.php
+++ cadastro.php
@@ -2,60 +2,193 @@
 <html lang="pt">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HeyEvent</title>
-    <link rel="stylesheet" href="cadastro.css">
-    <script src="https://kit.fontawesome.com/715a89f136.js" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>HeyEvent</title>
+   <link rel="stylesheet" href="cadastro.css">
+   <script src="https://kit.fontawesome.com/715a89f136.js" crossorigin="anonymous"></script>
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link
+      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet">
 </head>
 
 <body>
-    <aside>
-        <img class="Logo" src="assets/Logo HeyEvent Ofc.png" alt="" width="150px">
-        <div class="HE">
-            <p class="HE">HeyEvent</p>
+   <aside>
+      <img class="Logo" src="assets/Logo HeyEvent Ofc.png" alt="" width="150px">
+      <div class="HE">
+         <p>HeyEvent</p>
+      </div>
+      <div class="bvp">
+         <p>Bem-Vindo ao nosso portal!</p>
+      </div>
+      <div class="textos">
+         <p>Facilite a organização, comunicação e execução de eventos corporativos</p>
+         <p>Vizualize eventos futuros</p>
+         <p>Confirme sua presença rapidamente</p>
+         <p>Participe dos eventos e concorra a prêmios</p>
+      </div>
+   </aside>
+   <main>
+      <div class="topo">
+         <img class="logob" src="assets\Cadastro.png" alt="">
+         <h2>
+            <p>Cadastre-se</p>
+         </h2>
+      </div>
+      <form action="" method="post">
+         <div class="bdv">
+            <h3>Bem-Vindo a HeyEvent</h3>
+         </div>
+         <p class="p1">Preencha suas credênciais para acessar sua conta</p>
+         <input class="nome" type="text" name="nome" placeholder="Nome Completo">
+         <input class="email" type="email" name="email" placeholder="Email">
+         <input class="senha" type="text" name="senha" placeholder="Senha"> <br>
+         <select class="empresa" name="empresa">
+            <option value="" disabled selected>Selecione sua Empresa</option> <br>
+         </select>
+         <select class="tipoconta" name="tipoconta">
+            <option value="" disabled selected>Selecione o Tipo de Conta</option>
+         </select> <br>
+         <button type="submit">Entrar</button>
+         <p><a class="possuicnt" href="login.php">Já possui uma conta? Faça o Login aqui</a></p>
+         <p><a href="empresa.html">É Empresa? Faça seu Cadastro aqui</a></p>
+      </form>
 
-        </div>
-        <div class="textos">
-            <p>Bem-Vindo ao nosso portal!</p>
-            <p>Facilite a organização, comunicação e execução de eventos corporativos</p>
-            <p>Vizualize eventos futuros</p>
-            <p>Confirme sua presença rapidamente</p>
-            <p>Participe dos eventos e concorra a prêmios</p>
-        </div>
-    </aside>
-    <main>
-        <div class="topo">
-            <img class="logob" src="assets/Cadastro.png" alt="">
-            <h2>
-                <p>Cadastre-se</p>
-            </h2>
-        </div>
-        <form action="" method="post">
-            <div class="bdv">
-                <h3>Bem-Vindo a HeyEvent</h3>
-            </div>
-            <p class="p1">Preencha suas credênciais para acessar sua conta</p>
-            <input class="nome" type="text" name="nome" placeholder="Nome Completo">
-            <input class="email" type="email" name="email" placeholder="Email">
-            <input class="senha" type="text" name="senha" placeholder="Senha"> <br>
-            <select class="empresa" name="empresa">
-                <option value="" disabled selected>Selecione sua Empresa</option> <br>
-            </select>
-            <select class="tipoconta" name="tipoconta">
-                <option value="" disabled selected>Selecione o Tipo de Conta</option>
-            </select> <br>
-            <button type="submit">Entrar</button>
-            <p><a class="possuicnt" href="login.html">Já possui uma conta? Faça o Login aqui</a></p>
-            <p><a href="empresa.html">É Empresa? Faça seu Cadastro aqui</a></p>
-        </form>
+   </main>
+   <style>
+      body {
+         margin: 0;
+         padding: 0;
+         height: 100vh;
+         font-family: "Raleway", sans-serif;
 
-    </main>
+         background-image: linear-gradient(to bottom, #000F55, #6C0034);
+         background-repeat: no-repeat;
+         background-size: 50% 100%;
+      }
+
+      .Logo {
+         margin-top: 170px;
+      }
+
+      .HE {
+         color: white;
+         font-family: "Quicksand", sans-serif;
+         font-size: 90px;
+         margin-top: -100px;
+
+      }
+      .bvp{
+         color: white;
+         font-family: "Quicksand", sans-serif;
+         font-size: 50px;
+         margin-top: -100px;
+
+
+      }
+      aside {
+         color: rgb(255, 255, 255);
+      }
+
+      .textos {
+         margin-top: -40px;
+         font-size: 20px;
+
+      }
+
+      .asidep {
+         font-size: 40px;
+      }
+
+      main {
+
+         margin-left: 955px;
+         margin-top: -700px;
+      }
+
+      form {
+         border-radius: 5px;
+         width: 450px;
+         height: 650px;
+         text-align: center;
+         margin-left: 250px;
+         box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.108);
+
+      }
+
+      .topo {
+         margin-top: 10px;
+         text-align: center;
+      }
+
+      h2 {
+         color: #000F55;
+         margin-top: -40px;
+         margin-left: -10px;
+         text-align: center;
+         font-size: 30px;
+      }
+
+      .bdv {
+         color: #000F55;
+         padding: 20px;
+         font-size: 20px;
+      }
+
+      .p1 {
+         margin-top: -10px;
+      }
+
+      input {
+         height: 40px;
+         width: 300px;
+         border-radius: 8px;
+         border-style: solid;
+         border-color: black;
+         border-width: 1px;
+      }
+
+      .email {
+         margin-top: 30px;
+      }
+
+      .senha {
+         margin-top: 30px;
+      }
+
+      .logob {
+         width: 190px;
+      }
+
+      button {
+         margin-top: 20px;
+      }
+
+      select {
+         height: 43px;
+         width: 310px;
+         border-radius: 8px;
+         margin-top: 30px;
+         color: rgb(128, 128, 128);
+         border-color: black;
+      }
+
+      a {
+         text-decoration: none;
+         color: black;
+      }
+
+      button {
+         height: 40px;
+         width: 300px;
+         border-radius: 8px;
+         border-style: none;
+         background-color: #000F55;
+         color: white;
+      }
+   </style>
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -19,13 +19,15 @@
     <aside>
         <img class="Logo" src="assets/Logo HeyEvent Ofc.png" alt="" width="150px">
         <p class="HE">HeyEvent</p>
-    <div class="texto">
-        <p class="asidep">Bem-Vindo ao nosso portal!</p>
-        <p>Facilite a organização, comunicação e execução de eventos corporativos</p>
-        <p>Vizualize eventos futuros</p>
-        <p>Confirme sua presença rapidamente</p>
-        <p>Participe dos eventos e concorra a prêmios</p>
-    </div>
+        <div class="texto">
+            <div class="textos">
+                <p>Facilite a organização, comunicação e execução de eventos corporativos</p>
+                <p>Vizualize eventos futuros</p>
+                <p>Confirme sua presença rapidamente</p>
+                <p>Participe dos eventos e concorra a prêmios</p>
+
+            </div>
+        </div>
     </aside>
     <main>
         <div class="topo">
@@ -38,12 +40,13 @@
             <div class="bdv">
                 <h3>Bem-Vindo de Volta</h3>
             </div>
+            
             <p class="p1">Preencha suas credênciais para acessar sua conta</p>
             <input class="email" type="email" name="email" placeholder="Email"> <br>
             <input class="senha" type="senha" name="senha" placeholder="Senha">
             <p><a class="esqsenha" href="">Esqueceu a senha?</a></p>
             <button type="submit">Entrar</button>
-            <p><a class="npossuicnt" href="cadastro.html">Não possuí uma conta? Faça o cadastro aqui</a></p>
+            <p><a class="npossuicnt" href="cadastro.php">Não possuí uma conta? Faça o cadastro aqui</a></p>
         </form>
         <?php
         // if ($_POST) {
@@ -60,7 +63,121 @@
         //     $connection->close();
         // }
         ?>
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                height: 100vh;
+                font-family: "Raleway", sans-serif;
 
+                background-image: linear-gradient(to bottom, #000F55, #6C0034);
+                background-repeat: no-repeat;
+                background-size: 50% 100%;
+            }
+
+            .Logo {
+                margin-top: 200px;
+
+            }
+
+            .HE {
+                color: white;
+                font-family: "Quicksand", sans-serif;
+                font-size: 90px;
+                margin-top: -20px;
+            }
+
+            aside {
+                color: white;
+            }
+
+            .asidep {
+                font-size: 30px;
+            }
+
+            main {
+                margin-left: 955px;
+                margin-top: -750px;
+            }
+
+            .textos {
+                margin-top: -50px;
+                font-size: 20px;
+
+            }
+
+            form {
+                border-radius: 5px;
+                width: 450px;
+                height: 500px;
+                text-align: center;
+                margin-left: 250px;
+                box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.108);
+
+            }
+
+            .topo {
+                margin-top: 100px;
+                text-align: center;
+            }
+
+            h2 {
+                color: #000F55;
+                margin-top: -40px;
+                margin-left: -10px;
+                text-align: center;
+                font-size: 30px;
+            }
+
+            .bdv {
+                color: #000F55;
+                padding: 20px;
+                font-size: 20px;
+            }
+
+            .p1 {
+                margin-top: -10px;
+            }
+
+            input {
+                height: 40px;
+                width: 300px;
+                border-radius: 8px;
+                border-style: solid;
+            }
+
+            .senha {
+                margin-top: 30px;
+            }
+
+            .esqsenha {
+                margin-left: 150px;
+                color: #6C0034;
+
+            }
+
+            .npossuicnt {
+                font-size: 15px;
+                color: black;
+            }
+
+            a {
+                text-decoration: none;
+            }
+
+            .logob {
+                width: 150px;
+            }
+
+            button {
+                height: 40px;
+                width: 300px;
+                border-radius: 8px;
+                border-style: none;
+                background-color: #000F55;
+                color: white;
+            }
+        </style>
     </main>
 </body>
 
